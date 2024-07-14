51d57b4067227d4c9803882e3a6269fd40a8d139
MendozaEmerson <emendozahi@unsa.edu.pe>
2024-07-14 18:56:58 -0500

    Se logro reproducir solo los comandos play y pausa.

---
+++ app/src/main/java/com/quantumsoft/myapplication/Media/AudioPlayServices.kt
@@ -0,0 +1,120 @@
+package com.quantumsoft.myapplication.Media
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import android.util.Log
+
+class AudioPlayServices : Service() {
+    private var mediaPlayer: MediaPlayer? = null
+    private var isPaused: Boolean = false
+
+    companion object {
+        const val TAG = "AudioPlayServices"
+        const val FILENAME = "FILENAME"
+        const val COMMAND = "COMMAND"
+        const val PLAY = "PLAY"
+        const val PAUSE = "PAUSE"
+        const val RESUME = "RESUME"
+        const val STOP = "STOP"
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val filename = intent?.getStringExtra(FILENAME)
+        val command = intent?.getStringExtra(COMMAND)
+
+        Log.d(TAG, "onStartCommand: command=$command, filename=$filename")
+
+        when (command) {
+            PLAY -> {
+                if (!isPlaying()) {
+                    Log.d(TAG, "onStartCommand: Playing audio")
+                    audioPlay(filename)
+                } else {
+                    Log.d(TAG, "onStartCommand: Resuming audio")
+                    audioResume()
+                }
+            }
+            PAUSE -> {
+                Log.d(TAG, "onStartCommand: Pausing audio")
+                audioPause()
+            }
+            RESUME -> {
+                Log.d(TAG, "onStartCommand: Resuming audio")
+                audioResume()
+            }
+            STOP -> {
+                Log.d(TAG, "onStartCommand: Stopping audio")
+                audioStop()
+            }
+        }
+
+        return START_STICKY
+    }
+
+    private fun isPlaying(): Boolean {
+        return mediaPlayer?.isPlaying == true
+    }
+
+    private fun audioPlay(filename: String?) {
+        if (filename != null) {
+            try {
+                val assetFileDescriptor = assets.openFd(filename)
+                mediaPlayer = MediaPlayer()
+                mediaPlayer?.apply {
+                    setDataSource(
+                        assetFileDescriptor.fileDescriptor,
+                        assetFileDescriptor.startOffset,
+                        assetFileDescriptor.length
+                    )
+                    assetFileDescriptor.close()
+                    prepare()
+                    setVolume(1f, 1f)
+                    isLooping = false
+                    start()
+                    Log.d(TAG, "audioPlay: Started playing audio $filename")
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "audioPlay: Error playing audio", e)
+            }
+        } else {
+            Log.e(TAG, "audioPlay: Filename is null")
+        }
+        isPaused = false
+    }
+
+    private fun audioPause() {
+        mediaPlayer?.pause()
+        isPaused = true
+        Log.d(TAG, "audioPause: Paused audio")
+    }
+
+    private fun audioResume() {
+        if (isPaused) {
+            mediaPlayer?.start()
+            isPaused = false
+            Log.d(TAG, "audioResume: Resumed audio")
+        } else {
+            Log.d(TAG, "audioResume: Audio was not paused")
+        }
+    }
+
+    private fun audioStop() {
+        mediaPlayer?.apply {
+            stop()
+            release()
+        }
+        mediaPlayer = null
+        Log.d(TAG, "audioStop: Stopped audio")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        audioStop()
+    }
+}
--- app/src/main/java/com/quantumsoft/myapplication/fragments/StorytellingFragment.kt
+++ app/src/main/java/com/quantumsoft/myapplication/fragments/StorytellingFragment.kt
@@ -1,24 +1,18 @@
 package com.quantumsoft.myapplication.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.quantumsoft.myapplication.Media.AudioPlayServices
 import com.quantumsoft.myapplication.R
 
 class StorytellingFragment : Fragment() {
 
-    private var mParam1: String? = null
-    private var mParam2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            mParam1 = it.getString(ARG_PARAM1)
-            mParam2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var isPlaying = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,18 +22,43 @@ class StorytellingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_storytelling, container, false)
     }
 
-    companion object {
-        private const val ARG_PARAM1 = "param1"
-        private const val ARG_PARAM2 = "param2"
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val btnToggle: Button = view.findViewById(R.id.btn_toggle)
+
+        btnToggle.setOnClickListener {
+            if (isPlaying) {
+                // Pausar o detener la reproducción
+                btnToggle.setBackgroundResource(R.drawable.play_button_background)
+                btnToggle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_button_play, 0, 0, 0)
+                sendCommandToService(AudioPlayServices.PAUSE)
+            } else {
+                // Iniciar o reanudar la reproducción
+                btnToggle.setBackgroundResource(R.drawable.pause_button_background)
+                btnToggle.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_button_pause, 0, 0, 0)
+                sendCommandToService(AudioPlayServices.PLAY, "audioVoice2.mp3")
+            }
+            isPlaying = !isPlaying
+        }
+    }
+
+    private fun sendCommandToService(command: String, filename: String? = null) {
+        val audioPlayServiceIntent = Intent(requireContext(), AudioPlayServices::class.java)
+        audioPlayServiceIntent.putExtra(AudioPlayServices.COMMAND, command)
+        filename?.let {
+            audioPlayServiceIntent.putExtra(AudioPlayServices.FILENAME, it)
+        }
+        requireContext().startService(audioPlayServiceIntent)
+    }
+
+    companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             StorytellingFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
                 }
             }
     }
 }
-
